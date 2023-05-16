127932b2e8c4c0a83dec6eb3fe29b7458790d170
Cerisesss <ding@et.intechinfo.fr>
2023-05-16 14:06:54 +0200

    ajout des fichiers sql pour les tables

---
+++ produit.sql
@@ -0,0 +1,3 @@
+INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(250, 6, 'Fraises');
+INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(4, 6, 'Oeufs');
+INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(125, 6, 'Farine');
---
+++ unite.sql
@@ -0,0 +1,11 @@
+INSERT INTO unite(libelle_unite) VALUES('g');
+INSERT INTO unite(libelle_unite) VALUES('kg');
+INSERT INTO unite(libelle_unite) VALUES('ml');
+INSERT INTO unite(libelle_unite) VALUES('cl');
+INSERT INTO unite(libelle_unite) VALUES('l');
+INSERT INTO unite(libelle_unite) VALUES('c. à café');
+INSERT INTO unite(libelle_unite) VALUES('c. à soupe');
+INSERT INTO unite(libelle_unite) VALUES('pincée');
+INSERT INTO unite(libelle_unite) VALUES('gousse');
+INSERT INTO unite(libelle_unite) VALUES('verre');
+
