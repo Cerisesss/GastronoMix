0db037bdc4ed7722de637971fa8dd0170f40a47b
Cerisesss <ding@et.intechinfo.fr>
2023-05-17 11:20:33 +0200

    modif

--- produit.sql
+++ produit.sql
@@ -1,3 +1,3 @@
-INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(250, 6, 'Fraises');
-INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(4, 6, 'Oeufs');
-INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(125, 6, 'Farine');
+INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(250, 2, 'Fraises');
+INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(4, 2, 'Oeufs');
+INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(125, 2, 'Farine');
