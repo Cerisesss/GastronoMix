945673aeca9be5a596f4431a48df28acfd70e4ee
Cerisesss <ding@et.intechinfo.fr>
2023-05-17 14:33:37 +0200

    ajout de id_recette dans la table compose

---
+++ compose.sql
@@ -0,0 +1,3 @@
+INSERT INTO compose(id_recette, quantite_compose) VALUES(1, 250);
+INSERT INTO compose(id_recette, quantite_compose) VALUES(1, 4);
+INSERT INTO compose(id_recette, quantite_compose) VALUES(1, 125);
--- produit.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(250, 2, 'Fraises');
-INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(4, 2, 'Oeufs');
-INSERT INTO produit(quantite_produit, nb_personne_produit, libelle_produit) VALUES(125, 2, 'Farine');
