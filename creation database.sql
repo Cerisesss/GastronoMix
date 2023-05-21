f22923c7bd8e1801be9445b1940e1a66152af8c7
Cerisesss <ding@et.intechinfo.fr>
2023-05-21 22:51:22 +0200

    modif database

--- creation database.sql
+++ creation database.sql
@@ -23,7 +23,7 @@ CREATE TABLE recette (
 
 DROP TABLE IF EXISTS etape;
 CREATE TABLE etape (
-    id_etape BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id_etape BIGINT NOT NULL,
     nom_etape VARCHAR(50),
     texte_etape VARCHAR(400)
 );
@@ -45,36 +45,6 @@ CREATE TABLE unite (
     libelle_unite VARCHAR(50)
 );
 
-//
- DROP TABLE IF EXISTS unite ; 
-
-DROP TABLE IF EXISTS historique ; 
-CREATE TABLE historique (id_historique BIGINT AUTO_INCREMENT NOT NULL, avis_historique VARCHAR, favori_historique BIGINT, PRIMARY KEY (id_historique)) ENGINE=InnoDB;  
-
-DROP TABLE IF EXISTS categorie ; 
-CREATE TABLE categorie (id_c_categorie BIGINT AUTO_INCREMENT NOT NULL, libelle_categorie VARCHAR, sous_categorie_categorie VARCHAR, PRIMARY KEY (id_c_categorie)) ENGINE=InnoDB;  
-
-DROP TABLE IF EXISTS materiel ; 
-CREATE TABLE materiel (id_materiel BIGINT AUTO_INCREMENT NOT NULL, libelle_materiel VARCHAR, PRIMARY KEY (id_materiel)) ENGINE=InnoDB;  
-
-DROP TABLE IF EXISTS Pays ; 
-CREATE TABLE Pays (id_pays BIGINT AUTO_INCREMENT NOT NULL, nom VARCHAR, abrev_Pays VARCHAR, PRIMARY KEY (id_pays)) ENGINE=InnoDB;  
-
-DROP TABLE IF EXISTS possede ; 
-CREATE TABLE possede (id_historique **NOT FOUND** AUTO_INCREMENT NOT NULL, id_recette **NOT FOUND** NOT NULL, PRIMARY KEY (id_historique,  id_recette)) ENGINE=InnoDB;
-
-//
-
-
-
-DROP TABLE IF EXISTS produit ;
-CREATE TABLE produit (
-    id_produit BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    quantite_produit BIGINT,
-    nb_personne_produit BIGINT,
-    libelle_produit VARCHAR(50)
-);
-    
 
 DROP TABLE IF EXISTS historique;
 CREATE TABLE historique (
@@ -85,7 +55,7 @@ CREATE TABLE historique (
 
 DROP TABLE IF EXISTS categorie;
 CREATE TABLE categorie (
-    id_c_categorie BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id_categorie BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     libelle_categorie VARCHAR(50),
     sous_categorie VARCHAR(50)
 );
@@ -93,7 +63,7 @@ CREATE TABLE categorie (
 DROP TABLE IF EXISTS pays;
 CREATE TABLE pays (
     id_pays BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nom_pays VARCHAR(50),
+    nom VARCHAR(50),
     abrev_pays VARCHAR(50)
 );
     
@@ -103,44 +73,51 @@ CREATE TABLE materiel (
     libelle_materiel VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS contient ; 
+CREATE TABLE contient (
+    id_ingredient BIGINT NOT NULL,
+    id_unite BIGINT NOT NULL, 
+    PRIMARY KEY (id_ingredient,  id_unite)
+); 
+
 DROP TABLE IF EXISTS possede;
 CREATE TABLE possede (
-    id_historique BIGINT AUTO_INCREMENT NOT NULL,
+    id_historique BIGINT NOT NULL,
     id_recette BIGINT NOT NULL,
     PRIMARY KEY (id_historique, id_recette)
 );
 
 DROP TABLE IF EXISTS relier;
 CREATE TABLE relier (
-    id_compte BIGINT AUTO_INCREMENT NOT NULL,
+    id_compte BIGINT  NOT NULL,
     id_historique BIGINT NOT NULL,
     PRIMARY KEY (id_compte, id_historique)
 );
 
 DROP TABLE IF EXISTS consulter;
 CREATE TABLE consulter (
-    id_compte BIGINT AUTO_INCREMENT NOT NULL,
+    id_compte BIGINT NOT NULL,
     id_recette BIGINT NOT NULL,
     PRIMARY KEY (id_compte, id_recette)
 );
 
 DROP TABLE IF EXISTS decompose;
 CREATE TABLE decompose (
-    id_etape BIGINT AUTO_INCREMENT NOT NULL,
+    id_etape BIGINT NOT NULL,
     id_recette BIGINT NOT NULL,
     PRIMARY KEY (id_etape, id_recette)
 );
 
 DROP TABLE IF EXISTS categoriser;
 CREATE TABLE categoriser (
-    id_categorie BIGINT AUTO_INCREMENT NOT NULL,
+    id_categorie BIGINT NOT NULL,
     id_recette BIGINT NOT NULL,
     PRIMARY KEY (id_categorie, id_recette)
 );
 
 DROP TABLE IF EXISTS compose;
 CREATE TABLE compose (
-    id_ingredient BIGINT AUTO_INCREMENT NOT NULL,
+    id_ingredient BIGINT NOT NULL,
     id_recette BIGINT NOT NULL,
     quantite_compose BIGINT,
     PRIMARY KEY (id_ingredient, id_recette)
@@ -148,24 +125,18 @@ CREATE TABLE compose (
 
 DROP TABLE IF EXISTS utilise;
 CREATE TABLE utilise (
-    id_recette BIGINT AUTO_INCREMENT NOT NULL,
+    id_recette BIGINT NOT NULL,
     id_materiel BIGINT NOT NULL,
     PRIMARY KEY (id_recette, id_materiel)
 );
 
 DROP TABLE IF EXISTS provient;
 CREATE TABLE provient (
-    id_recette BIGINT AUTO_INCREMENT NOT NULL,
+    id_recette BIGINT NOT NULL,
     id_Pays BIGINT NOT NULL,
     PRIMARY KEY (id_recette,  id_Pays)
 );
 
-DROP TABLE IF EXISTS contient;
-CREATE TABLE contient (
-    id_ingredient BIGINT NOT NULL,
-    id_unite BIGINT NOT NULL,
-    PRIMARY KEY (id_ingredient, id_unite)
-);
 
 ALTER TABLE contient ADD CONSTRAINT FK_contient_id_ingredient FOREIGN KEY (id_ingredient) REFERENCES ingredient (id_ingredient);
 ALTER TABLE contient ADD CONSTRAINT FK_contient_id_unite FOREIGN KEY (id_unite) REFERENCES unite (id_unite);
@@ -177,7 +148,7 @@ ALTER TABLE consulter ADD CONSTRAINT FK_consulter_id_compte FOREIGN KEY (id_comp
 ALTER TABLE consulter ADD CONSTRAINT FK_consulter_id_recette FOREIGN KEY (id_recette) REFERENCES recette (id_recette);
 ALTER TABLE decompose ADD CONSTRAINT FK_decompose_id_etape FOREIGN KEY (id_etape) REFERENCES etape (id_etape);
 ALTER TABLE decompose ADD CONSTRAINT FK_decompose_id_recette FOREIGN KEY (id_recette) REFERENCES recette (id_recette);
-ALTER TABLE categoriser ADD CONSTRAINT FK_categoriser_id FOREIGN KEY (id_categorie) REFERENCES categorie (id_categorie);
+ALTER TABLE categoriser ADD CONSTRAINT FK_categoriser_id_categorie FOREIGN KEY (id_categorie) REFERENCES categorie (id_categorie);
 ALTER TABLE categoriser ADD CONSTRAINT FK_categoriser_id_recette FOREIGN KEY (id_recette) REFERENCES recette (id_recette);
 ALTER TABLE compose ADD CONSTRAINT FK_compose_id_ingredient FOREIGN KEY (id_ingredient) REFERENCES ingredient (id_ingredient);
 ALTER TABLE compose ADD CONSTRAINT FK_compose_id_recette FOREIGN KEY (id_recette) REFERENCES recette (id_recette);
