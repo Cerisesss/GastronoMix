a4bd7c7beadf3a58a6fb7a84f24e19629ab7a079
Cerisesss <ding@et.intechinfo.fr>
2023-06-06 17:52:56 +0200

    page recette (ca affiche biennnnnnnn yessssssss)

--- recette.php
+++ recette.php
@@ -44,30 +44,65 @@
             if (isset($_GET['recherche'])) {
                 $mot_clef = $_GET['recherche'];
 
-                $query = "SELECT r.image_recette, r.titre, r.temps_prep_recette, r.temps_cui_recette, r.temps_repos_recette, m.libelle_materiel, r.nb_personne, i.nom_ingredient, q.quantite, u.libelle_unite, e.id_etape, e.texte_etape
-                        FROM recette r
-                        JOIN etape e ON e.id_recette = r.id_recette
-                        JOIN quantite q ON q.id_recette = r.id_recette
-                        JOIN ingredient i ON i.id_ingredient = q.id_ingredient
-                        JOIN unite u ON u.id_unite = i.id_unite
+                $query = "SELECT image_recette, titre, temps_prep_recette, temps_cui_recette, temps_repos_recette, nb_personne
+                        FROM recette 
+                        WHERE titre LIKE '%$mot_clef%';";
+
+
+                $result_recette = $mysqli->query($query);
+
+
+                $row = mysqli_fetch_assoc($result_recette);
+                echo $row['image_recette'] . "</br>";
+                echo "<h2>" . $row["titre"] . "</h2></br>";
+                echo "<p>Temps de preparation : </p>" . $row['temps_prep_recette']. "</p>";
+                echo "<p>Temps de cuisson : </p>" . $row['temps_cui_recette']. "</p>";
+                echo "<p>Temps de repos : </p>" . $row['temps_repos_recette'] . "</p>";
+                echo "<h3>Nombre de personne</h3>" . $row['nb_personne'] . "</br>" ;
+
+
+                $query = "SELECT m.libelle_materiel FROM recette r
                         JOIN recette_materiel rm ON rm.id_recette = r.id_recette
                         JOIN materiel m ON m.id_materiel = rm.id_materiel
                         WHERE r.titre LIKE '%$mot_clef%';";
 
 
-                $result = $mysqli->query($query);
+                $result_materiel = $mysqli->query($query);
+
+                echo "<h3>Materiel</h3>";
+                
+                while ($row = mysqli_fetch_assoc($result_materiel)) {
+                    echo "<li>" . $row['libelle_materiel'] . "</li>" ; 
+                }
 
 
-                $row = mysqli_fetch_assoc($result);
-                echo $row['image_recette'] . "</br>";
-                echo "<h2>" . $row["titre"] . "</h2>";
-                echo "<p>Temps de preparation : </p>" . $row['temps_prep_recette']. "</p>" . "</br>";
-                echo "<p>Temps de cuisson : </p>" . $row['temps_cui_recette']. "</p>" . "</br>" ;
-                echo "<p>Temps de repos : </p>" . $row['temps_repos_recette'] . "</p>" . "</br>" ;
-                echo "<h3>Materiel</h3>" . $row['libelle_materiel'] . "</br>" ; 
-                echo "<h3>Nombre de personne</h3>" . $row['nb_personne'] . "</br>" ;
-                echo "<h3>Ingredient</h3>" . $row['nom_ingredient'] . " " . $row['quantite'] . " " . $row['libelle_unite'] . "</br>";
-                echo "<h3>Etape</h3>" .  $row['id_etape'] . " :	" . $row['texte_etape'] . "</br>";
+                $query = "SELECT i.nom_ingredient, q.quantite, u.libelle_unite FROM recette r
+                        JOIN quantite q ON q.id_recette = r.id_recette
+                        JOIN ingredient i ON i.id_ingredient = q.id_ingredient
+                        JOIN unite u ON u.id_unite = i.id_unite
+                        WHERE r.titre LIKE '%$mot_clef%';";
+
+
+                $result_ingredient = $mysqli->query($query);
+
+                echo "<h3>Ingredient</h3>";
+                
+                while ($row = mysqli_fetch_assoc($result_ingredient)) {
+                    echo "<li>" . $row['nom_ingredient'] . " " . $row['quantite'] . " " . $row['libelle_unite'] . "</li>" ; 
+                }
+
+
+                $query = "SELECT e.id_etape, e.texte_etape FROM recette r 
+                        JOIN etape e ON e.id_recette = r.id_recette
+                        WHERE r.titre LIKE '%$mot_clef%';";
+
+                $result_ingredient = $mysqli->query($query);
+
+                echo "<h3>Etape</h3>";
+                
+                while ($row = mysqli_fetch_assoc($result_ingredient)) {
+                    echo "<li>" . "Etape " . $row['id_etape'] . " : " . $row['texte_etape'] . "</li>" ; 
+                }
 
                 echo "</br>";
             } else {
