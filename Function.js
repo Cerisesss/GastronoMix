cf24d6195497e2ef8dffc446d9ea1b5fc974146a
Léa <pedra@et.intechinfo.fr>
2023-06-24 23:25:42 +0200

    j'ai oublié d'ajouter les nouvelles fonctions que j'ai ajouté

--- Function.js
+++ Function.js
@@ -16,14 +16,43 @@ function toggleCompte() {
     }
 }
 
-
 function ChangeBackgroundColor() {
     var html = document.documentElement;
     backgroundColor = html.style.backgroundColor;
 
-    if (backgroundColor === 'white' || backgroundColor === '') {
-        html.style.backgroundColor = '#202020';
+    if (backgroundColor === '#white' || backgroundColor === '') {
+        html.style.backgroundColor = '#d9b9b9';
     } else {
-        html.style.backgroundColor = 'white';
+        html.style.backgroundColor = '#white';
     }
 }
+
+function telecharger_image(url, new_image) {
+    return new Promise((resolve, reject) => {
+      const dirPath = path.resolve(__dirname, 'images_recettes');
+      if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath);
+      }
+  
+      const filePath = path.resolve(dirPath, new_image + '.jpg');
+      const file = fs.createWriteStream(filePath);
+      const request = https.get(url, function (response) {
+        response.pipe(file);
+        file.on('finish', function () {
+          file.close();
+          resolve(filePath);
+        });
+      }).on('error', function (err) {
+        fs.unlink(filePath, () => {
+          reject(err);
+        });
+      });
+    });
+  }
+  module.exports = telecharger_image;
+
+  function fichierExiste(filePath) {
+    return fs.existsSync(filePath);
+  }
+
+    module.exports = fichierExiste;
