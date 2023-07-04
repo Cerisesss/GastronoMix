04d9f91d61965e5bf52c1243f1ff93cd25b81407
Léa <pedra@et.intechinfo.fr>
2023-07-05 00:03:09 +0200

    ajout recette ' plus le trim

--- resultat_recherche_avancee.php
+++ resultat_recherche_avancee.php
@@ -49,6 +49,8 @@
 
                 // Récupérer les mots associés à "aliments_ok", "aliments_ko" et "categorie"
                 if (!empty($_GET['aliments_ok']) || !empty($_GET['aliments_ko']) || !empty($_GET['categorie'])) {
+                    echo "<h3>Recettes correspondant à votre recherche :</h3>";
+
                     //regarde si les variables existent et si elles ne sont pas vides
                     if (!empty($_GET['aliments_ok'])) {
                         $aliments_ok = $_GET['aliments_ok'];
@@ -66,8 +68,9 @@
                         $categorie = "";
                     }
 
-                    //test de récupération des données
-                    //var_dump($_GET['aliments_ok'], $_GET['aliments_ko'], $_GET['categorie']);
+                    // test de récupération des données
+                    // echo "aliments_ok : et aliments_ko : et categorie :";
+                    // var_dump($_GET['aliments_ok'], $_GET['aliments_ko'], $_GET['categorie']);
 
                     // Convertir les tableaux en chaînes de caractères
                     if (!empty($aliments_ok)) {
@@ -88,38 +91,38 @@
 
                     // Requête SQL pour récupérer les recettes correspondantes
                     $sql = "SELECT DISTINCT r.titre, r.image_recette, c.libelle_categorie
-                                FROM recette r
-                                JOIN quantite q ON q.id_recette = r.id_recette
-                                JOIN ingredient i ON i.id_ingredient = q.id_ingredient
-                                JOIN categorie c ON c.id_categorie = r.id_categorie
-                                WHERE ";
-
-                    // Si les deux tableaux ne sont pas vides, on ajoute les deux conditions
-                    if (!empty($aliments_ok_string) && !empty($aliments_ko_string)) {
-                        $sql .= "i.ingredients_recherche IN ('$aliments_ok_string') AND
-                            NOT EXISTS (
+                    FROM recette r
+                    JOIN quantite q ON q.id_recette = r.id_recette
+                    JOIN ingredient i ON i.id_ingredient = q.id_ingredient
+                    JOIN categorie c ON c.id_categorie = r.id_categorie
+                    WHERE ";
+            
+            // Si les deux tableaux ne sont pas vides, on ajoute les deux conditions
+            if (!empty($aliments_ok_string) && !empty($aliments_ko_string)) {
+                $sql .= "TRIM(i.ingredients_recherche) IN ('$aliments_ok_string') AND
+                    NOT EXISTS (
+                    SELECT 1
+                    FROM quantite q2
+                    JOIN ingredient i2 ON i2.id_ingredient = q2.id_ingredient
+                    WHERE q2.id_recette = r.id_recette
+                    AND TRIM(i2.ingredients_recherche) IN ('$aliments_ko_string')
+                    )";
+            } else if (!empty($aliments_ok_string) && empty($aliments_ko_string)) { 
+                // Sinon, on ajoute la condition correspondante            
+                $sql .= "TRIM(i.ingredients_recherche) = ('$aliments_ok_string')"; 
+            } else if (!empty($aliments_ko_string) && empty($aliments_ok_string)) {
+                // Idem pour les aliments à exclure            
+                $sql .= "NOT EXISTS (
                             SELECT 1
                             FROM quantite q2
                             JOIN ingredient i2 ON i2.id_ingredient = q2.id_ingredient
                             WHERE q2.id_recette = r.id_recette
-                            AND i2.ingredients_recherche IN ('$aliments_ko_string')
-                            )";
-                    } else if (!empty($aliments_ok_string) && empty($aliments_ko_string)) { 
-                        // Sinon, on ajoute la condition correspondante
-                        $sql .= "i.ingredients_recherche IN ('$aliments_ok_string')"; 
-                    } else if (!empty($aliments_ko_string) && empty($aliments_ok_string)) {
-                        // Idem pour les aliments à exclure
-                        $sql .= "NOT EXISTS (
-                                        SELECT 1
-                                        FROM quantite q2
-                                        JOIN ingredient i2 ON i2.id_ingredient = q2.id_ingredient
-                                        WHERE q2.id_recette = r.id_recette
-                                        AND i2.ingredients_recherche IN ('$aliments_ko_string')
-                                    )";
-                    } else {
-                        // Aucun filtre sur les aliments
-                        $sql .= "1"; 
-                    }
+                            AND TRIM(i2.ingredients_recherche) IN ('$aliments_ko_string')
+                        )";
+            } else {
+                // Aucun filtre sur les aliments
+                $sql .= "1"; 
+            }
 
                     // Ajouter la condition sur la catégorie si la personne à pas mis une catégorie spécifique
                     if ($categorie == 'null') {
@@ -136,15 +139,16 @@
                     // Utilisez le résultat de la requête pour afficher les données
                     if ($result && $result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
+                            $titre = $row['titre'];
+                            $newtitre = str_replace("'", "_", $titre);
                             echo '<div class="recette zoom">';
                             // Image cliquable
-                            echo '<a href="http://localhost/gastronomix/recette.php?recherche=' . urlencode($row['titre']) . '">';
+                            echo '<a href="http://localhost/gastronomix/recette.php?recherche=' . urlencode($newtitre) . '">';
                             echo '<img src="' . $row['image_recette'] . '" alt="Recette">';
                             echo '</a>';
                             echo '<div class="nom-recette">';
                             // Titre cliquable
-                            echo '<a href="http://localhost/gastronomix/recette.php?recherche=' . urlencode($row['titre']) . '">';
-                            echo $row['titre'];
+                            echo '<a href="http://localhost/gastronomix/recette.php?recherche=' . urlencode($newtitre) . '">' . $titre . '</a>';
                             echo '</a>';
                             echo '</div>';
                             echo '</div>';
